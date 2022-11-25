85563abbcff50a4938b92f65c393abfbed9bb996
CatJason <CatJason@github.com>
2022-11-25 20:54:22 +0800

    创建 void 类型注入函数，入参为待注入依赖的实例，之后 rebuild project
    (cherry picked from commit 6cefa3e61ca2e7ffdc5bb0d32d406a64ad080f26)
    (cherry picked from commit 66341e7dfded9da608386b018bd2694b623bcef8)

--- app/src/main/java/com/bilibili/dagger/sevice/component/BiliServiceComponent.kt
+++ app/src/main/java/com/bilibili/dagger/sevice/component/BiliServiceComponent.kt
@@ -1,9 +1,10 @@
 package com.bilibili.dagger.sevice.component
 
+import com.bilibili.dagger.MainActivity
 import com.bilibili.dagger.sevice.module.BiliServiceModule
 import dagger.Component
 
 @Component(modules = [BiliServiceModule::class])
 abstract class BiliServiceComponent {
-
+    abstract fun inject(activity: MainActivity)
 }
