dd95cdd0ee6b3f092628177304fb2e75e893e8e0
CatJason <CatJason@github.com>
2022-11-25 21:01:28 +0800

    在 MainActivity 中合适的时机调用 Dagger 生成的 DaggerBiliServiceComponent 完成依赖注入
    (cherry picked from commit 21d806c3ca8361d4bd0968885c32fcbb113c6db2)
    (cherry picked from commit f8237fa75fff9f84cff462b5588036c506904ef7)

--- app/src/main/java/com/bilibili/dagger/MainActivity.kt
+++ app/src/main/java/com/bilibili/dagger/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.bilibili.dagger.sevice.BiliService
+import com.bilibili.dagger.sevice.component.DaggerBiliServiceComponent
+import com.bilibili.dagger.sevice.module.BiliServiceModule
 import dagger.android.DaggerActivity
 import dagger.android.DaggerActivity_MembersInjector
 import javax.inject.Inject
@@ -15,5 +17,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        DaggerBiliServiceComponent.builder().biliServiceModule(BiliServiceModule()).build().inject(this)
     }
 }
