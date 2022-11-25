907b1babc0e8c886c6818fe901a0633c8b641ef6
CatJason <CatJason@github.com>
2022-11-25 20:09:49 +0800

    在 MainActivity 里面用 @Inject 声明一个需要注入的 BiliService 成员变量
    (cherry picked from commit c561e28cb8b1b6e383e06f02096ef462a6acc6e3)

--- app/src/main/java/com/bilibili/dagger/MainActivity.kt
+++ app/src/main/java/com/bilibili/dagger/MainActivity.kt
@@ -2,8 +2,16 @@ package com.bilibili.dagger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.bilibili.dagger.sevice.BiliService
+import dagger.android.DaggerActivity
+import dagger.android.DaggerActivity_MembersInjector
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var biliService: BiliService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
