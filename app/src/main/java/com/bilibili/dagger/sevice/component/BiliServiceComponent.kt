a5e5832ad001723e857fb945cb84e2439d15d7cc
CatJason <CatJason@github.com>
2022-11-25 20:52:21 +0800

    设置 BiliServiceComponent 联接的 Module 为对应的 BiliServiceModule
    (cherry picked from commit 9bf8accfbda68c2f6c2beb0eece0cfc1ce8d71a5)
    (cherry picked from commit c2c74aaf7342fc5295b8d21ec22defb906631703)

--- app/src/main/java/com/bilibili/dagger/sevice/component/BiliServiceComponent.kt
+++ app/src/main/java/com/bilibili/dagger/sevice/component/BiliServiceComponent.kt
@@ -3,7 +3,7 @@ package com.bilibili.dagger.sevice.component
 import com.bilibili.dagger.sevice.module.BiliServiceModule
 import dagger.Component
 
-@Component
+@Component(modules = [BiliServiceModule::class])
 abstract class BiliServiceComponent {
 
 }
