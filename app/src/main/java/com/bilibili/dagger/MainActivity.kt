d2237c20dfad91f9c272b0d37182cf94f152cb44
CatJason <CatJason@github.com>
2022-11-25 21:03:37 +0800

    简化代码，了解 DaggerBiliServiceComponent create 函数做了什么
    (cherry picked from commit 2906a2eab1597cb04b9cdcddda0976aba2aabe12)
    (cherry picked from commit 462f80a71d8f12565d9678a2eabe53208e0804e5)

--- app/src/main/java/com/bilibili/dagger/MainActivity.kt
+++ app/src/main/java/com/bilibili/dagger/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        DaggerBiliServiceComponent.builder().biliServiceModule(BiliServiceModule()).build().inject(this)
+        // DaggerBiliServiceComponent.builder().biliServiceModule(BiliServiceModule()).build().inject(this)
+        DaggerBiliServiceComponent.create().inject(this)
     }
 }
