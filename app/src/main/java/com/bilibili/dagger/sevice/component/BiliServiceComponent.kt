43b89848659ebe5ba38f145c924416a73efeed9b
CatJason <CatJason@github.com>
2022-11-25 20:21:38 +0800

    创建虚类 BiliServiceComponent 作为依赖注入的管道
    (cherry picked from commit acd190fe0d2f2de13b1d74f44b93b7ec6bbd97a6)
    (cherry picked from commit f118cfbdcec31fae13cd6ef44a660df871e3a67d)

---
+++ app/src/main/java/com/bilibili/dagger/sevice/component/BiliServiceComponent.kt
@@ -0,0 +1,9 @@
+package com.bilibili.dagger.sevice.component
+
+import com.bilibili.dagger.sevice.module.BiliServiceModule
+import dagger.Component
+
+@Component
+abstract class BiliServiceComponent {
+
+}
