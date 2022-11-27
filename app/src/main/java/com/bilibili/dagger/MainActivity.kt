dfed9336e231e44e51f647bd1592ceaa5052d8bd
CatJason <CatJason@github.com>
2022-11-27 18:14:24 +0800

    查看在构造方法上使用 @Injet 后 BiliServiceComponent 中的 provideBiliService 实际做了什么
    (cherry picked from commit c3d1e4732b55ac0747c6e6e7a000776c474aa682)
    (cherry picked from commit 982357e757f95a35340bacc95cc62b41707ad32c)
    (cherry picked from commit 7487a9207f6d4897ba7608baa55a3e2defc84a62)

--- app/src/main/java/com/bilibili/dagger/MainActivity.kt
+++ app/src/main/java/com/bilibili/dagger/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // DaggerBiliServiceComponent.builder().biliServiceModule(BiliServiceModule()).build().inject(this)
-        biliService = DaggerBiliServiceComponent.create().provideBiliService()
+        // biliService = DaggerBiliServiceComponent.create().provideBiliService()
+        biliService = BiliService()
     }
 }
