eec46d1099ff8f0b58384149ba74361f446391c7
CatJason <CatJason@github.com>
2022-11-28 04:18:06 +0800

    简化代码，拒绝耦合，模块化的好处
    (cherry picked from commit d22e6556a605fa076a2e6c0489adc932bd405c3a)
    (cherry picked from commit 81ec05a6484821c5d5c524b49a46fb69efdf29ec)
    (cherry picked from commit f1de5266f361386cb12578fdfe5a248c76301382)

--- app/src/main/java/com/bilibili/dagger/MainActivity.kt
+++ app/src/main/java/com/bilibili/dagger/MainActivity.kt
@@ -15,10 +15,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val builder = DaggerBiliServiceComponent
-            .builder()
-            .biliServiceModule(BiliServiceModule())
-            .build()
-        builder.inject(this)
+        DaggerBiliServiceComponent.create().inject(this)
     }
 }
