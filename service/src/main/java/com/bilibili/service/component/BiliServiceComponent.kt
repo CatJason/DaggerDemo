c0b924e810e13dc6255a33ebf9393155b6d28031
CatJason <CatJason@github.com>
2022-12-01 18:17:01 +0800

    @Component.Factory types must have exactly one abstract method. Already found: create()

--- service/src/main/java/com/bilibili/service/component/BiliServiceComponent.kt
+++ service/src/main/java/com/bilibili/service/component/BiliServiceComponent.kt
@@ -3,14 +3,26 @@ package com.bilibili.service.component
 import com.bilibili.reportservice.component.BilliReportServiceComponent
 import com.bilibili.service.BiliService
 import com.bilibili.service.module.BiliServiceModule
+import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Named
 
-@Component(modules = [BiliServiceModule::class], dependencies = [BilliReportServiceComponent::class])
+@Component(modules = [BiliServiceModule::class])
 interface BiliServiceComponent {
     @Named("danmaku")
     fun provideBiliDanmakuService(): BiliService
 
     @Named("player")
     fun provideBiliPlayerService(): BiliService
+
+    @Component.Factory
+    interface Factory {
+        fun create(): BiliServiceComponent
+
+        @BindsInstance
+        fun provideBiliDanmakuService(@Named("danmaku") biliService: BiliService): Factory
+
+        @BindsInstance
+        fun provideBiliPlayerService(@Named("player") biliService: BiliService): Factory
+    }
 }
