9b763939125b7e57644c43cc41a40b145e7bfa2e
CatJason <CatJason@github.com>
2022-12-05 16:29:20 +0800

    error: com.bilibili.dagger.component.MainActivityComponent (unscoped) cannot depend on scoped components
    (cherry picked from commit dbaf71971043c55185a548128fb7de242185c8cc)

--- service/src/main/java/com/bilibili/service/component/BiliServiceComponent.kt
+++ service/src/main/java/com/bilibili/service/component/BiliServiceComponent.kt
@@ -1,16 +1,16 @@
 package com.bilibili.service.component
 
 import com.bilibili.reportservice.component.BilliReportServiceComponent
+import com.bilibili.service.BiliServiceScope
 import com.bilibili.service.BiliService
-import com.bilibili.service.BiliServiceMapKey
 import com.bilibili.service.module.BiliServiceModule
 import dagger.Component
-import javax.inject.Named
 
 @Component(
     modules = [BiliServiceModule::class],
     dependencies = [BilliReportServiceComponent::class]
 )
+@BiliServiceScope
 interface BiliServiceComponent {
     fun provideBiliServices(): Map<String, BiliService>
 }
--- service/src/main/java/com/bilibili/service/module/BiliServiceModule.kt
+++ service/src/main/java/com/bilibili/service/module/BiliServiceModule.kt
@@ -1,21 +1,19 @@
 package com.bilibili.service.module
 
-import com.bilibili.reportservice.BiliDanmakuReportService
 import com.bilibili.reportservice.BiliReportService
-import com.bilibili.reportservice.module.BiliReportServiceModule
+import com.bilibili.service.BiliServiceScope
 import com.bilibili.service.BiliService
 import com.bilibili.service.BiliServiceMapKey
 import dagger.Module
 import dagger.Provides
 import dagger.multibindings.IntoMap
-import dagger.multibindings.IntoSet
-import dagger.multibindings.StringKey
 import javax.inject.Named
 
 @Module
 class BiliServiceModule {
     @Provides
     @IntoMap
+    @BiliServiceScope
     @BiliServiceMapKey("danmaku")
     fun provideBiliDanmakuService(@Named("danmakuReport") biliReportService: BiliReportService): BiliService {
         return BiliService(biliReportService)
@@ -23,6 +21,7 @@ class BiliServiceModule {
 
     @Provides
     @IntoMap
+    @BiliServiceScope
     @BiliServiceMapKey("player")
     fun provideBiliPlayerService(@Named("playerReport") biliReportService: BiliReportService): BiliService {
         return BiliService(biliReportService)
