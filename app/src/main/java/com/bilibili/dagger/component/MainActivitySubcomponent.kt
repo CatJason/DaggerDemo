f265f5d7592ef531fe2a9a5281eb8fcdd38d95a6
CatJason <CatJason@github.com>
2022-12-05 16:44:18 +0800

    error: com.bilibili.dagger.component.MainActivityComponent (unscoped) cannot depend on scoped components

--- app/src/main/java/com/bilibili/dagger/component/MainActivitySubcomponent.kt
+++ app/src/main/java/com/bilibili/dagger/component/MainActivitySubcomponent.kt
@@ -2,8 +2,10 @@ package com.bilibili.dagger.component
 
 import com.bilibili.dagger.MainActivity
 import com.bilibili.dagger.module.MainActivityComponentModule
+import com.bilibili.service.BiliServiceScope
 import dagger.Subcomponent
 
+@BiliServiceScope
 @Subcomponent
 interface MainActivitySubcomponent {
     fun inject(mainActivity: MainActivity)
