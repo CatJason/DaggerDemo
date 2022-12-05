440461289f47fbaa2271393a332241b83bc600d8
CatJason <CatJason@github.com>
2022-12-05 16:45:09 +0800

    error: com.bilibili.dagger.component.MainActivityComponent depends on scoped components in a non-hierarchical scope ordering

--- app/src/main/java/com/bilibili/dagger/component/MainActivityComponent.kt
+++ app/src/main/java/com/bilibili/dagger/component/MainActivityComponent.kt
@@ -2,11 +2,13 @@ package com.bilibili.dagger.component
 
 import com.bilibili.dagger.MainActivity
 import com.bilibili.dagger.module.MainActivityComponentModule
+import com.bilibili.service.BiliServiceScope
 import com.bilibili.service.component.BiliServiceComponent
 import com.bilibili.service.module.BiliServiceModule
 import dagger.Component
 import dagger.Subcomponent
 
+@BiliServiceScope
 @Component(modules = [MainActivityComponentModule::class], dependencies = [BiliServiceComponent::class])
 interface MainActivityComponent {
     fun getSubComponentBuilder(): MainActivitySubcomponent.Builder
